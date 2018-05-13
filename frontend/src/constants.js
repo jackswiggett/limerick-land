25aadc40d9364ef178d01b6040010079da2c8818
Jack Swiggett <jackswiggett@gmail.com>
2018-05-13 16:23:08 -0700

    Prepare for deployment to Heroku and Surge

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = 'http://localhost:3100';
+export const API_URL = process.env.NODE_ENV === 'production'
+  ? 'https://limerick-land.herokuapp.com'
+  : 'http://localhost:3100';
