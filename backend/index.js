56ed7849440b12507246f11038f97096b595d496
Jack Swiggett <jackswiggett@gmail.com>
2018-05-23 15:01:26 -0700

    Update randpoem endpoint to use $sample and return a line id

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
-const cors = require('cors');
-const express = require('express');
-const boolParser = require('express-query-boolean');
-const mongoose = require('mongoose');
-const ValidatedLine = require('./models/line').ValidatedLine;
-const UnvalidatedLine = require('./models/line').UnvalidatedLine;
+const cors = require("cors");
+const express = require("express");
+const boolParser = require("express-query-boolean");
+const mongoose = require("mongoose");
+const ValidatedLine = require("./models/line").ValidatedLine;
+const UnvalidatedLine = require("./models/line").UnvalidatedLine;
 
 const app = express();
 app.use(express.json());
@@ -12,58 +12,58 @@ app.use(cors());
 
 /* If deployed in production get the URI of the mLab database. Otherwise,
  * use the local database. */
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/limerick-land';
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/limerick-land";
 mongoose.connect(MONGODB_URI);
 
 // create a new first line
-app.post('/firstline', async (req, res) => {
+app.post("/firstline", async (req, res) => {
   const Line = req.body.validateLines ? ValidatedLine : UnvalidatedLine;
 
   const line = new Line({
     text: req.body.text,
     index: 0,
-    userId: req.body.userId,
+    userId: req.body.userId
   });
   await line.save();
   res.end();
 });
 
 // get all existing first lines
-app.get('/firstline', async (req, res) => {
+app.get("/firstline", async (req, res) => {
   const Line = req.query.validateLines ? ValidatedLine : UnvalidatedLine;
 
-  const firstLines = await Line
-    .find({ index: 0 })
-    .select('text')
+  const firstLines = await Line.find({ index: 0 })
+    .select("text")
     .sort({ createdAt: -1 }) // show recently added lines first
     .exec();
   res.json(firstLines);
 });
 
 // create a new line that is a child of an existing line
-app.post('/line', async (req, res) => {
+app.post("/line", async (req, res) => {
   const Line = req.body.validateLines ? ValidatedLine : UnvalidatedLine;
 
   const parent = await Line.findById(req.body.parentId).exec();
   if (parent.index === 4) {
-    response.status(403).end('Cannot have a limerick with more than 5 lines.');
+    response.status(403).end("Cannot have a limerick with more than 5 lines.");
   }
 
   const line = new Line({
     text: req.body.text,
     index: parent.index + 1,
-    userId: req.body.userId,
+    userId: req.body.userId
   });
   await line.save();
-  
+
   parent.children.push(line._id);
   await parent.save();
 
-  res.end('Success');
+  res.end("Success");
 });
 
 // get a line along with its ancestors and children
-app.get('/line', async (req, res) => {
+app.get("/line", async (req, res) => {
   const Line = req.query.validateLines ? ValidatedLine : UnvalidatedLine;
 
   const line = await Line.findById(req.query.lineId).exec();
@@ -77,17 +77,20 @@ app.get('/line', async (req, res) => {
    * 'children' will be an array of objects, e.g.
    *   [{ text: 'some line' }, { text: 'some other line' }]
    */
-  const children = await Promise.all(line.children.map(async (childId) => {
-    const child = await Line.findById(childId).select('text').exec();
-    return child;
-  }));
+  const children = await Promise.all(
+    line.children.map(async childId => {
+      const child = await Line.findById(childId)
+        .select("text")
+        .exec();
+      return child;
+    })
+  );
 
   // Get all the ancestors of this line, up until we reach the first line
   const ancestors = [line];
   while (ancestors[0].index !== 0) {
-    const parent = await Line
-      .findOne({ children: ancestors[0]._id })
-      .select('text index')
+    const parent = await Line.findOne({ children: ancestors[0]._id })
+      .select("text index")
       .exec();
     ancestors.splice(0, 0, parent);
   }
@@ -97,31 +100,25 @@ app.get('/line', async (req, res) => {
   res.json({
     ...line.toObject(),
     children,
-    ancestors,
+    ancestors
   });
-})
-    
-app.get('/randpoem', async (req, res) => {
-  const Line = ValidatedLine;
-  const lastLines = await Line.find({ index: 4 }).exec();
-  if (lastLines.length === 0) return res.json({});
-  const idx = Math.floor((Math.random() * lastLines.length));
-  const line = lastLines[idx];
-  // Get all the ancestors of this line, up until we reach the first line
-  const ancestors = [line];
-  while (ancestors[0].index !== 0) {
-    const parent = await Line
-      .findOne({ children: ancestors[0]._id })
-      .select('text index')
-      .exec();
-    ancestors.splice(0, 0, parent);
-  }
-  
+});
+
+app.get("/randpoem", async (req, res) => {
+  const Line = req.query.validateLines ? ValidatedLine : UnvalidatedLine;
+
+  const lastLine = await Line.aggregate([
+    { $match: { index: 4 } },
+    { $sample: { size: 1 } }
+  ]);
+
   res.json({
-    ancestors,
+    lineId: lastLine[0]._id,
   });
-})
+});
 
 const port = process.env.PORT || 3100;
 
-app.listen(port, () => console.log(`limerick-land-server listening on port ${port}`));
+app.listen(port, () =>
+  console.log(`limerick-land-server listening on port ${port}`)
+);
