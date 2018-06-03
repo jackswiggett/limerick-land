77e0339acb7b1e9e8b356c00f9ba2b1afc0c41f5
James Lyons <jlyons6100@gmail.com>
2018-06-03 02:38:16 -0700

    Throttled Rhyme Checking with bad formatting and no color changing

---
+++ frontend/src/generic.js
@@ -0,0 +1,20 @@
+import styled from "tachyons-components";
+
+export const Section = styled('div')`
+flex flex-wrap content-center justify-center w-300 h-40 bg-white`;
+
+export const Article = styled('div')`
+w-10 ma2 h1 items-center justify-center flex flex-column flex-wrap`;
+
+export const Prop = styled('h3')`
+f5 f4-ns mb0 white`;
+
+export const Title = styled('h1') `
+f4 f3-ns white w-10 tc`;
+
+export const list = [
+  {
+    prop: "spin",
+    name: "Spin"
+  }
+];
